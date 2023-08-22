94f435b49021b6dd54e87a6ba91ef974118cd9cc
Shirogane Tsumugi <1036015366@qq.com>
2023-08-22 16:13:15 +0800

    correct hero data of those fail to grab earlier.

--- src/App.vue
+++ src/App.vue
@@ -9,71 +9,73 @@
       :isRedSide="true" /> -->
 
       <v-expand-transition>
-        <v-container style="max-width: 999999px" v-show="display">
-          <v-row no-gutters>
-            <v-col cols="4">
-              <v-container style="padding: 0px">
-                <v-row no-gutters>
-                  <v-col
-                    cols="2"
-                    v-for="(hname, idx) in red_ban"
-                    :key="'red-ban-' + idx"
-                  >
-                    <hero-display :heroname="hname" :isRedSide="true" />
-                  </v-col>
-                  <v-col cols="2">
-                    <hero-display :heroname="'ban'" :isRedSide="true" />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-col>
-            <v-col cols="4">
-              <!-- spacer -->
-            </v-col>
-            <v-col cols="4">
-              <v-container style="padding: 0px">
-                <v-row no-gutters>
-                  <v-col cols="2"> </v-col>
-                  <v-col cols="2">
-                    <hero-display :heroname="'ban'" :isRedSide="false" />
-                  </v-col>
-                  <v-col
-                    cols="2"
-                    v-for="(hname, idx) in blue_ban"
-                    :key="'blue-ban-' + idx"
-                  >
-                    <hero-display :heroname="hname" :isRedSide="false" />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-col>
-          </v-row>
-          <v-row no-gutters>
-            <v-col
-              cols="1"
-              v-for="(hname, idx) in red_hero_stat"
-              :key="'red-' + idx"
-            >
-              <hero-display :heroname="hname" :isRedSide="true" />
-            </v-col>
-            <v-col cols="2">
-              <match-display
-                :matchname="matchname"
-                :teamleft="teamleft"
-                :scoreleft="scoreleft"
-                :teamright="teamright"
-                :scoreright="scoreright"
-              />
-            </v-col>
-            <v-col
-              cols="1"
-              v-for="(hname, idx) in blue_hero_stat"
-              :key="'blue-' + idx"
-            >
-              <hero-display :heroname="hname" :isRedSide="false" />
-            </v-col>
-          </v-row>
-        </v-container>
+        <div v-show="display" style="margin:10px">
+          <v-container style="max-width: 999999px">
+            <v-row no-gutters>
+              <v-col cols="4">
+                <v-container style="padding: 0px">
+                  <v-row no-gutters>
+                    <v-col
+                      cols="2"
+                      v-for="(hname, idx) in red_ban"
+                      :key="'red-ban-' + idx"
+                    >
+                      <hero-display :heroname="hname" :isRedSide="true" />
+                    </v-col>
+                    <v-col cols="2">
+                      <hero-display :heroname="'ban'" :isRedSide="true" />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-col>
+              <v-col cols="4">
+                <!-- spacer -->
+              </v-col>
+              <v-col cols="4">
+                <v-container style="padding: 0px">
+                  <v-row no-gutters>
+                    <v-col cols="2"> </v-col>
+                    <v-col cols="2">
+                      <hero-display :heroname="'ban'" :isRedSide="false" />
+                    </v-col>
+                    <v-col
+                      cols="2"
+                      v-for="(hname, idx) in blue_ban"
+                      :key="'blue-ban-' + idx"
+                    >
+                      <hero-display :heroname="hname" :isRedSide="false" />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col
+                cols="1"
+                v-for="(hname, idx) in red_hero_stat"
+                :key="'red-' + idx"
+              >
+                <hero-display :heroname="hname" :isRedSide="true" />
+              </v-col>
+              <v-col cols="2">
+                <match-display
+                  :matchname="matchname"
+                  :teamleft="teamleft"
+                  :scoreleft="scoreleft"
+                  :teamright="teamright"
+                  :scoreright="scoreright"
+                />
+              </v-col>
+              <v-col
+                cols="1"
+                v-for="(hname, idx) in blue_hero_stat"
+                :key="'blue-' + idx"
+              >
+                <hero-display :heroname="hname" :isRedSide="false" />
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
       </v-expand-transition>
     </div>
 
@@ -129,6 +131,10 @@
                 ></v-text-field>
 
               </v-card-text>
+
+              <v-card-actions>
+                <v-btn @click="display = !display" text>{{display?"隐藏":"显示"}}</v-btn>
+              </v-card-actions>
             </v-card>
           </v-col>
 
@@ -148,9 +154,9 @@
               </v-card-text>
               <v-divider />
               <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn @click="display = !display" text>{{display?"隐藏":"显示"}}</v-btn>
-                <v-btn @click="clearhero()" text>一键重开</v-btn>
+                <v-btn @click="clearhero()" text>重开</v-btn>
+                <v-btn @click="switchteam()" text>交换</v-btn>
+
               </v-card-actions>
             </v-card>
           </v-col>
@@ -202,101 +208,7 @@ export default {
   },
 
   data: () => ({
-    all_hero: [
-      "云中君",
-      "瑶",
-      "盘古",
-      "猪八戒",
-      "嫦娥",
-      "上官婉儿",
-      "李信",
-      "沈梦溪",
-      "伽罗",
-      "盾山",
-      "司马懿",
-      "孙策",
-      "元歌",
-      "米莱狄",
-      "狂铁",
-      "弈星",
-      "裴擒虎",
-      "杨玉环",
-      "公孙离",
-      "明世隐",
-      "女娲",
-      "梦奇",
-      "苏烈",
-      "百里玄策",
-      "百里守约",
-      "铠",
-      "鬼谷子",
-      "干将莫邪",
-      "东皇太一",
-      "大乔",
-      "黄忠",
-      "诸葛亮",
-      "哪吒",
-      "太乙真人",
-      "蔡文姬",
-      "雅典娜",
-      "杨戬",
-      "成吉思汗",
-      "钟馗",
-      "虞姬",
-      "李元芳",
-      "张飞",
-      "刘备",
-      "后羿",
-      "牛魔",
-      "孙悟空",
-      "亚瑟",
-      "橘右京",
-      "娜可露露",
-      "不知火舞",
-      "张良",
-      "花木兰",
-      "兰陵王",
-      "王昭君",
-      "韩信",
-      "刘邦",
-      "姜子牙",
-      "露娜",
-      "程咬金",
-      "安琪拉",
-      "貂蝉",
-      "关羽",
-      "老夫子",
-      "武则天",
-      "项羽",
-      "达摩",
-      "狄仁杰",
-      "马可波罗",
-      "李白",
-      "宫本武藏",
-      "典韦",
-      "曹操",
-      "甄姬",
-      "夏侯惇",
-      "周瑜",
-      "吕布",
-      "芈月",
-      "白起",
-      "扁鹊",
-      "孙膑",
-      "钟无艳",
-      "阿轲",
-      "高渐离",
-      "刘禅",
-      "庄周",
-      "鲁班七号",
-      "孙尚香",
-      "嬴政",
-      "妲己",
-      "墨子",
-      "赵云",
-      "小乔",
-      "廉颇",
-    ],
+    all_hero: ["亚连", "姬小满", "莱西奥", "赵怀真", "海月", "戈娅", "桑启", "暃", "金蝉", "云缨", "艾琳", "司空震", "澜", "夏洛特", "阿古朵", "蒙恬", "镜", "蒙犽", "鲁班大师", "西施", "马超", "曜", "云中君", "瑶", "盘古", "猪八戒", "嫦娥", "上官婉儿", "李信", "沈梦溪", "伽罗", "盾山", "司马懿", "孙策", "元歌", "米莱狄", "狂铁", "弈星", "裴擒虎", "杨玉环", "公孙离", "明世隐", "女娲", "梦奇", "苏烈", "百里玄策", "百里守约", "铠", "鬼谷子", "干将莫邪", "东皇太一", "大乔", "黄忠", "诸葛亮", "哪吒", "太乙真人", "蔡文姬", "雅典娜", "杨戬", "成吉思汗", "钟馗", "虞姬", "李元芳", "张飞", "刘备", "后羿", "牛魔", "孙悟空", "亚瑟", "橘右京", "娜可露露", "不知火舞", "张良", "花木兰", "兰陵王", "王昭君", "韩信", "刘邦", "姜子牙", "露娜", "程咬金", "安琪拉", "貂蝉", "关羽", "老夫子", "武则天", "项羽", "达摩", "狄仁杰", "马可波罗", "李白", "宫本武藏", "典韦", "曹操", "甄姬", "夏侯惇", "周瑜", "吕布", "芈月", "白起", "扁鹊", "孙膑", "钟无艳", "阿轲", "高渐离", "刘禅", "庄周", "鲁班七号", "孙尚香", "嬴政", "妲己", "墨子", "赵云", "小乔", "廉颇"],
     red_picked: [],
     red_banned: [],
     blue_picked: [],
@@ -320,6 +232,15 @@ export default {
       this.blue_picked = []
       this.blue_banned = []
     },
+    switchteam(){
+      let temp = this.teamleft
+      this.teamleft = this.teamright
+      this.teamright = temp
+
+      temp = this.scoreleft
+      this.scoreleft = this.scoreright
+      this.scoreright = temp
+    }
   },
 
   watch:{
@@ -382,9 +303,10 @@ export default {
 <style>
 .render_area {
   background-color: rgb(0, 255, 0);
-  height: 300px;
+  height: 350px;
   width: 100%;
   padding: 20px;
+  z-index: 99999;
 }
 
 .no_scroll {
